9adcf53e2709aea985b1caceeb3807155a69c09a
leilanivquesada <62270422+leilanivquesada@users.noreply.github.com>
2021-04-17 14:47:48 -0700

    created 2 additional blocks
    sort title and sort array

--- sort_blocks.rb
+++ sort_blocks.rb
@@ -1,6 +1,3 @@
-The ruby array sort method can use a block to sort various arrays. The block must compare between two elements of the array, a and b. If a<b, it should evaluate to -1. If a==b, it should evaluate to 0. And if a>b, it should evaluate to 1. Here is the start of a program, which you should call sort_blocks.rb.
-
-
 class Book
   attr_reader :author, :title, :count
   def initialize(author,title,count)
@@ -20,7 +17,7 @@ book_array.push(Book.new("Bob Woodward","All the President's Men",30))
 
 puts "Sorting alphabetically by author"
 
-new_array = book_array.sort do |a,b|
+sorted_author_array = book_array.sort do |a,b|
   author1 = a.author.downcase
   author2 = b.author.downcase
   author1 <=> author2
@@ -32,22 +29,19 @@ new_array = book_array.sort do |a,b|
   #   0
   # end
 end
-puts new_array
 
-new_title_array = book.array.sort do | a, b |
-
-def sorted_by_author(array, author)
-	array.sort do |a, b|
-		author1 = author1.downcase
-		author2 = author2.downcase
-		author1 <=> author2
-	end
+sorted_title_array = book_array.sort do |a,b|
+	title1 = a.title.downcase
+	title2 = b.title.downcase
+	title1 <=> title2
 end
 
-def sorted_by_title(array, title)
-
+sorted_count_array = book_array.sort do |a,b|
+	count1 = a.count
+	count2 = b.count
+	count1 <=> count2
 end
 
-def sorted_by_count
-	end
-
+print "Sorted Author Array: #{sorted_author_array}"
+print "Sorted Title Array: #{sorted_title_array}"
+print "Sorted Count Array: #{sorted_count_array}"
